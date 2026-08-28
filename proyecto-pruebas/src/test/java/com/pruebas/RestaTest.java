5fcecb455cf25724a345dda007c9fcbeecdb2d0e
Nato <natoalvarado07@gmail.com>
2026-08-28 13:59:59 -0400

    feat: agrega proyecto Maven y pruebas unitarias

---
+++ proyecto-pruebas/src/test/java/com/pruebas/RestaTest.java
@@ -0,0 +1,13 @@
+package com.pruebas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RestaTest {
+
+    @Test
+    public void testResta() {
+        int resultado = 5 - 2;
+        assertEquals(3, resultado);
+    }
+}
---
+++ proyecto-pruebas/src/test/java/com/pruebas/SumaTest.java
@@ -0,0 +1,13 @@
+package com.pruebas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SumaTest {
+
+    @Test
+    public void testSuma() {
+        int resultado = 2 + 3;
+        assertEquals(5, resultado);
+    }
+}
